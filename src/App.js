10fc534f4190f195c2f83eaba06e69838739f3e7
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-09 11:33:35 +0200

    Add basic setup

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import './App.scss';
 function App() {
   return (
     <div className="page-wrapper">
-     
+      <div className="content-wrapper">
+
+        <header>
+          <h1 className="heading-primary">
+            SPLI
+            <span className="line-breaker">TTER</span>
+          </h1>
+        </header>
+
+        <main className="main-content">
+
+        </main>
+
+      </div>
     </div>
+
   );
 }
 
