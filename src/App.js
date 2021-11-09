260161d46b9561b9383c45de2e7bf45a39bdb194
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-09 13:16:57 +0200

    Add number of people input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import './App.scss';
 import BillInput from './components/BillInput';
+import PeopleInput from './components/PeopleInput';
 
 function App() {
   return (
+
     <div className="page-wrapper">
-      <div className="content-wrapper">
+      <header>
+        <h1 className="heading-primary">
+          SPLI
+          <span className="line-breaker">TTER</span>
+        </h1>
+      </header>
 
-        <header>
-          <h1 className="heading-primary">
-            SPLI
-            <span className="line-breaker">TTER</span>
-          </h1>
-        </header>
+      <main className="main-content">
+        <div className="content-wrapper">
 
-        <main className="main-content">
           <BillInput />
-        </main>
+          <PeopleInput />
+        </div>
 
-      </div>
+      </main>
     </div>
 
   );
---
+++ src/components/BillInput.js
@@ -0,0 +1,15 @@
+const BillInput = () => {
+    return (
+        <>
+            <h2 className="heading-secondary">Bill</h2>
+            <div className="input-container">
+                <input type="text" className="text-input" />
+                <div className="input-icon-container">
+                    <img src="/images/icon-dollar.svg" alt="Small grey dollar icon" className="input-icon" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default BillInput;
